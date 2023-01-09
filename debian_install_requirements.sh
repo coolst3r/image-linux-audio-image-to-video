69269123dc19cca5a14974fc05b99006944aca03
skiddo <30941141+coolst3r@users.noreply.github.com>
2023-01-09 00:42:41 +0000

    Update debian_install_requirements.sh

--- debian_install_requirements.sh
+++ debian_install_requirements.sh
@@ -6,8 +6,17 @@ if ! command -v python3 &>/dev/null; then
     sudo apt-get install python3
 fi
 
-# Check if GTK+ 3 is installed
-if ! pkg-config --exists gtk+-3.0; then
-    # Install GTK+ 3
-    sudo apt-get install python3-gi gir1.2-gtk-3.0
+# Check if pip is installed
+if ! command -v pip &>/dev/null; then
+    # Install pip
+    sudo apt-get install python3-pip
+fi
+
+# Install the ffmpeg-python package
+pip install ffmpeg-python
+
+# Check if ffmpeg is installed
+if ! command -v ffmpeg &>/dev/null; then
+    # Install ffmpeg
+    sudo apt-get install ffmpeg
 fi
